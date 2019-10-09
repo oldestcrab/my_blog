d081baf84aca2196c62aed4d68437234f8a9b2f7
oldestcrab <oldestcrab@gmail.com>
2019-10-09 22:10:22 +0800

    用户注册完成

--- accounts/views.py
+++ accounts/views.py
@@ -1,31 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .forms import RegisterForm
 
+from .forms import RegisterForm
 from .models import Profile
 
 def register(request):
+    """
+    注册视图
+    :param request:
+    :return:
+    """
     if request.method == 'POST':
         # 传递post数据
         reg_form = RegisterForm(request.POST)
         # 数据验证
         if reg_form.is_valid():
             username = reg_form.cleaned_data['username']
-            nickname = reg_form.cleaned_data['nickname']
             email = reg_form.cleaned_data['email']
             password = reg_form.cleaned_data['password']
             # 保存用户
             user = User.objects.create_user(username, email, password)
-            profile = Profile(user=user)
-            profile.nickname = nickname
-            profile.save()
+            # 如果有昵称，则保存
+            nickname = reg_form.cleaned_data['nickname']
+            if nickname:
+                profile = Profile(user=user)
+                profile.nickname = nickname
+                profile.save()
+            # 跳转到登录页面
+            return redirect('home')
     else:
         # 初始化注册表单
         reg_form = RegisterForm()
 
     context = {
         'title': '注册',
-        'reg_form': reg_form,
+        'form': reg_form,
     }
 
     return render(request, 'accounts/forms.html', context=context)
